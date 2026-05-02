d4e1163226f9b06869ae151377536c267a472475
shaon07 <alishaon078@gmail.com>
2026-05-02 17:13:40 +0600

    feat: add logging setup with tracing and tower-http for improved request tracing

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,35 @@ mod routes;
 mod controllers;
 mod services;
 mod entity;
+mod utils;
 
 use axum::Router;
 use dotenvy::dotenv;
 use tokio::net::TcpListener;
-use crate::routes::UserRoute;
+use crate::{ routes::UserRoute, utils::init_logger };
 
 #[tokio::main]
 async fn main() {
+    init_logger();
+
     dotenv().ok();
+    tracing::info!("env files loaded");
 
     let db = db::connect_to_db().await;
+    tracing::info!("Databased connected");
 
     let state = state::AppState {
         db,
     };
+    tracing::info!("global state created");
 
-    let app = Router::new().merge(UserRoute::create_user_routes()).with_state(state);
+    let app = Router::new()
+        .merge(UserRoute::create_user_routes())
+        .with_state(state)
+        .layer(tower_http::trace::TraceLayer::new_for_http());
+    tracing::info!("routes created");
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-    println!("Server running on http://localhost:3000");
-
-    axum::serve(listener, app).await.unwrap()
+    tracing::info!("🚀 Server running on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
 }
