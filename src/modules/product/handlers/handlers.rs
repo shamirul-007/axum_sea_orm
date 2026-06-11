022635eca5ce4fb5d5494317d6e91cddfd761ae4
shaon07 <alishaon078@gmail.com>
2026-06-11 23:47:07 +0600

    feat: added get product api route

--- src/modules/product/handlers/handlers.rs
+++ src/modules/product/handlers/handlers.rs
@@ -1,15 +1,15 @@
-use crate::state::AppState;
-use crate::utils::{ ApiResponse, AppError, ValidatedJson };
-use axum::Json;
-use axum::extract::State;
 use crate::modules::product::dto::request::create_product_dto::CreateProductDto;
 use crate::modules::product::dto::response::product_response_dto::ProductResponseDto;
 use crate::modules::product::services::service::ProductService;
+use crate::state::AppState;
+use crate::utils::{ApiResponse, AppError, ValidatedJson};
+use axum::Json;
+use axum::extract::{Path, State};
+use uuid::Uuid;
 
-pub async fn get_products(State(state): State<AppState>) -> Result<
-    Json<ApiResponse<Vec<ProductResponseDto>>>,
-    AppError
-> {
+pub async fn get_products(
+    State(state): State<AppState>,
+) -> Result<Json<ApiResponse<Vec<ProductResponseDto>>>, AppError> {
     let products = ProductService::new(state.db).get_products().await?;
 
     Ok(Json(ApiResponse::success(products)))
@@ -17,8 +17,16 @@ pub async fn get_products(State(state): State<AppState>) -> Result<
 
 pub async fn add_product(
     State(state): State<AppState>,
-    ValidatedJson(data): ValidatedJson<CreateProductDto>
+    ValidatedJson(data): ValidatedJson<CreateProductDto>,
 ) -> Result<Json<ApiResponse<Option<ProductResponseDto>>>, AppError> {
     let product = ProductService::new(state.db).add_product(data).await?;
+    Ok(Json(ApiResponse::success(product)))
+}
+
+pub async fn get_product(
+    State(state): State<AppState>,
+    Path(id): Path<Uuid>,
+) -> Result<Json<ApiResponse<Option<ProductResponseDto>>>, AppError> {
+    let product = ProductService::new(state.db).get_product_by_id(id).await?;
     Ok(Json(ApiResponse::success(product)))
 }
--- src/modules/product/routes/routes.rs
+++ src/modules/product/routes/routes.rs
@@ -1,8 +1,10 @@
+use crate::modules::product::handlers::handlers::{add_product, get_product, get_products};
 use crate::state::AppState;
 use axum::Router;
 use axum::routing::get;
-use crate::modules::product::handlers::handlers::{add_product, get_products};
 
 pub fn product_routes() -> Router<AppState> {
-    Router::new().route("/", get(get_products).post(add_product))
+    Router::new()
+        .route("/", get(get_products).post(add_product))
+        .route("/:id", get(get_product))
 }
